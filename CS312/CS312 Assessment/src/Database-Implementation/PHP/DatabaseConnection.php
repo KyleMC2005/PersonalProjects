60e63113660801141e5cc576d0c34de2cf905383
Kyle McCann <kyleextra12@gmail.com>
2025-11-05 19:56:03 +0000

    First initialisation of Project

---
+++ CS312/CS312 Assessment/src/Database-Implementation/PHP/DatabaseConnection.php
@@ -0,0 +1,26 @@
+<?php
+//Connect to MySQL
+$host = "devweb2025.cis.strath.ac.uk";//Host to get from
+$user = "mjb23137";// My DS Username
+$pass = "lf56yWb58v$";//My MySQL password
+$dbname = $user;
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error){
+    die("Connection failed : ".$conn->connect_error); //FIXME remove once working - leaks database info so increasing hacking chances.
+}
+
+//Issue the query
+$sql = "SELECT * FROM `table`";//note do not have `abc01234`. in front of the table name if you used $dbname to connect
+$result = $conn->query($sql);
+
+if (!$result){
+    die("Query failed ".$conn->error); //FIXME remove once working.
+}
+
+//Handle the results
+echo "<p>".$result->num_rows." rows found</p>";
+
+//Disconnect
+$conn->close();
+?>
